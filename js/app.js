9714ec1469d5eb3833dcd939c86142055f60d3bc
Alex <jzsplking@gmail.com>
2018-04-13 18:38:50 +0800

    Feature: 增加测试页面，用与直接建立mqtt连接

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 /*eslint_d-env node */
 //这里是我们游戏中的一些常数
-// var WIDTH = 101,
-// HEIGHT = 83,
-// BASIC_SPEED = 100,
-// HighScore = 0;
-const MQTT_URI = "http://192.168.1.30/";
+const MQTT_URI = "tcp://www.iqi1.com:18843";
 const BASE_URL = "https://www.iqi1.com/";
 const URL_LOGIN_WEIXIN = "api/auth/AuthWith?RefSource=wechat"; //wechat登录
 const URL_TOKEN_UPDATE = "api/auth/updatetoken"; //更新token
@@ -29,26 +25,159 @@ const URL_UPDATE_ADDRESS = "api/addr/update";
 const URL_REMOVE_ADDRESS = "api/addr/remove";
 const URL_DEF_ADDRESS = "api/addr/def";  //设置默认地址
 
+connected = false;
 
 // Create a client instance
-var client = new Paho.MQTT.Client('q.emqtt.com', Number(8083), "clientId");
+function initMQTTClient() {
+      var hostname = document.getElementById("hostInput").value;
+      var port = document.getElementById("portInput").value;
+      var clientId = document.getElementById("clientIdInput").value;
 
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+      var path = document.getElementById("pathInput").value;
+      var user = document.getElementById("userInput").value;
+      var pass = document.getElementById("passInput").value;
+      var keepAlive = Number(document.getElementById("keepAliveInput").value);
+      var timeout = Number(document.getElementById("timeoutInput").value);
+      var tls = document.getElementById("tlsInput").checked;
+      var cleanSession = document.getElementById("cleanSessionInput").checked;
+      var lastWillTopic = document.getElementById("lwtInput").value;
+      var lastWillQos = Number(document.getElementById("lwQosInput").value);
+      var lastWillRetain = document.getElementById("lwRetainInput").checked;
+      var lastWillMessage = document.getElementById("lwMInput").value;
 
-// connect the client
-client.connect({onSuccess:onConnect});
+
+      if(path.length > 0){
+        client = new Paho.MQTT.Client(hostname, Number(port), path, clientId);
+      } else {
+        client = new Paho.MQTT.Client(hostname, Number(port), clientId);
+      }
+      console.info('Connecting to Server: Hostname: ', hostname, '. Port: ', port, '. Path: ', client.path, '. Client ID: ', clientId);
+
+      // set callback handlers
+      client.onConnectionLost = onConnectionLost;
+      client.onMessageArrived = onMessageArrived;
+
+
+      var options = {
+        invocationContext: {host : hostname, port: port, path: client.path, clientId: clientId},
+        timeout: timeout,
+        keepAliveInterval:keepAlive,
+        cleanSession: cleanSession,
+        useSSL: tls,
+        onSuccess: onConnect,
+        onFailure: onFail
+      };
+
+
+
+      if(user.length > 0){
+        options.userName = user;
+      }
+
+      if(pass.length > 0){
+        options.password = pass;
+      }
+
+      if(lastWillTopic.length > 0){
+        var lastWillMessage = new Paho.MQTT.Message(lastWillMessage);
+        lastWillMessage.destinationName = lastWillTopic;
+        lastWillMessage.qos = lastWillQos;
+        lastWillMessage.retained = lastWillRetain;
+        options.willMessage = lastWillMessage;
+      }
+
+      // connect the client
+      client.connect(options);
+      var statusSpan = document.getElementById("connectionStatus");
+      statusSpan.innerHTML = 'Connecting...';
+  
+}
+
+//disconnect 
+function disconnect(){
+    console.info('Disconnecting from Server');
+    client.disconnect();
+    var statusSpan = document.getElementById("connectionStatus");
+    statusSpan.innerHTML = 'Connection - Disconnected.';
+    connected = false;
+    setFormEnabledState(false);
+
+}
+
+//onFail
+function onFail(context) {
+  console.log("Failed to connect");
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Failed to connect: " + context.errorMessage;
+  connected = false;
+  setFormEnabledState(false);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("Connection Lost: " + responseObject.errorMessage);
+  }
+  connected = false;
+}
+
+// Sets various form controls to either enabled or disabled
+function setFormEnabledState(enabled){
+
+    // Connection Panel Elements
+    if(enabled){
+      document.getElementById("clientConnectButton").innerHTML = "Disconnect";
+    } else {
+      document.getElementById("clientConnectButton").innerHTML = "Connect";
+    }
+    document.getElementById("hostInput").disabled = enabled;
+    document.getElementById("portInput").disabled = enabled;
+    document.getElementById("clientIdInput").disabled = enabled;
+    document.getElementById("pathInput").disabled = enabled;
+    document.getElementById("userInput").disabled = enabled;
+    document.getElementById("passInput").disabled = enabled;
+    document.getElementById("keepAliveInput").disabled = enabled;
+    document.getElementById("timeoutInput").disabled = enabled;
+    document.getElementById("tlsInput").disabled = enabled;
+    document.getElementById("cleanSessionInput").disabled = enabled;
+    document.getElementById("lwtInput").disabled = enabled;
+    document.getElementById("lwQosInput").disabled = enabled;
+    document.getElementById("lwRetainInput").disabled = enabled;
+    document.getElementById("lwMInput").disabled = enabled;
+
+    // Publish Panel Elements
+    document.getElementById("publishTopicInput").disabled = !enabled;
+    document.getElementById("publishQosInput").disabled = !enabled;
+    document.getElementById("publishMessageInput").disabled = !enabled;
+    document.getElementById("publishButton").disabled = !enabled;
+    document.getElementById("publishRetainInput").disabled = !enabled;
+
+    // Subscription Panel Elements
+    document.getElementById("subscribeTopicInput").disabled = !enabled;
+    document.getElementById("subscribeQosInput").disabled = !enabled;
+    document.getElementById("subscribeButton").disabled = !enabled;
+    document.getElementById("unsubscribeButton").disabled = !enabled;
+
+}
+
+// var client = new Paho.MQTT.Client(MQTT_URI, Number(8083), "clientId");
+
+// // set callback handlers
+// client.onConnectionLost = onConnectionLost;
+// client.onMessageArrived = onMessageArrived;
+
+// // connect the client
+// client.connect({onSuccess:onConnect});
 
 
 // called when the client connects
-function onConnect() {
+function onConnect(context) {
   // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("World");
-  var message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
+  console.log("Client Connected");
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Connected to: " + context.invocationContext.host + ':' + context.invocationContext.port + context.invocationContext.path + ' as ' + context.invocationContext.clientId;
+  connected = true;
+  setFormEnabledState(true);
 }
 
 // called when the client loses its connection
@@ -58,9 +187,56 @@ function onConnectionLost(responseObject) {
   }
 }
 
+// // called when a message arrives
+// function onMessageArrived(message) {
+//   console.log("onMessageArrived:"+message.payloadString);
+// }
+
 // called when a message arrives
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log('Message Recieved: Topic: ', message.destinationName, '. Payload: ', message.payloadString, '. QoS: ', message.qos);
+  console.log(message);
+  var messageTime = new Date().toISOString();
+  // Insert into History Table
+  var table = document.getElementById("incomingMessageTable").getElementsByTagName('tbody')[0];
+  var row = table.insertRow(0);
+  row.insertCell(0).innerHTML = message.destinationName;
+  row.insertCell(1).innerHTML = safe_tags_regex(message.payloadString);
+  row.insertCell(2).innerHTML = messageTime;
+  row.insertCell(3).innerHTML = message.qos;
+
+
+  if(!document.getElementById(message.destinationName)){
+      var lastMessageTable = document.getElementById("lastMessageTable").getElementsByTagName('tbody')[0];
+      var newlastMessageRow = lastMessageTable.insertRow(0);
+      newlastMessageRow.id = message.destinationName;
+      newlastMessageRow.insertCell(0).innerHTML = message.destinationName;
+      newlastMessageRow.insertCell(1).innerHTML = safe_tags_regex(message.payloadString);
+      newlastMessageRow.insertCell(2).innerHTML = messageTime;
+      newlastMessageRow.insertCell(3).innerHTML = message.qos;
+
+  } else {
+      // Update Last Message Table
+      var lastMessageRow = document.getElementById(message.destinationName);
+      lastMessageRow.id = message.destinationName;
+      lastMessageRow.cells[0].innerHTML = message.destinationName;
+      lastMessageRow.cells[1].innerHTML = safe_tags_regex(message.payloadString);
+      lastMessageRow.cells[2].innerHTML = messageTime;
+      lastMessageRow.cells[3].innerHTML = message.qos;
+  }
+
+}
+
+//connection Toggle
+function connectionToggle(){
+
+  if(connected){
+    disconnect();
+  } else {
+    initMQTTClient();
+  }
+
+
 }
 
 
